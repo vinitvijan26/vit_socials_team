8b698d3e02eb183aed6adf507e30557338163015
Vinit Vijan <vinit26virgo@gmail.com>
2018-01-19 21:21:19 +0530

    Initial commit

---
+++ team (1).php
@@ -0,0 +1,210 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="css/pausingscroller.css" />
+        <link rel="stylesheet" href="css/w3.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="description" content="This is the official website of 'The VIT Socials' of VIT,Pune">
+        <meta name="author" content="Pratik Kharade, Varun Gujarathi, Aditi Katare, Pradnya Jori">
+        <link rel="icon" href="./img/socials.jpg" />
+        <link rel="stylesheet" type="text/css" href="./css/main.css" />
+        <script language="javascript" src="./js/main.js"></script>
+      	<!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!--font-awesome-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<!--google fonts/icons-->        
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- jQuery CDN -->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.5.1/jquery.nicescroll.min.js"></script>
+        <?php include "niceScroll.php";?>
+        <!-- Google font alegreya sans -->
+        <link href='https://fonts.googleapis.com/css?family=Alegreya Sans' rel='stylesheet'>
+        
+        <title>Meet the Team | VIT Socials</title>
+    </head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="50" onscroll="scrollFunction()">
+<!-- Navigation bar -->
+	<nav id="nav" class="navbar navbar-inverse navbar-fixed-top"> <!-- data-spy="affix" data-offset-top="200" -->
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	    <!--menubutton for mobileview-->
+	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">         	  
+	       <i class="material-icons" style="color: #fff;border:#fff;">menu</i>
+	      </button>
+
+	      <a class="navbar-brand" href="index.php" style="color:#eee;"><p style="font-size: 20px;"><img id="nav_img" src="img/socials.jpg" width="50px" class="img-rounded" style="top:0px;"> VIT Socials</p></a>  <!--club name on nav bar-->
+	    </div>
+	    <div class="collapse navbar-collapse"  id="myNavbar">
+	    		<ul class="nav navbar-nav navbar-right">
+	        		<li><a href="director.php"><h4>Director's Desk</h4></a></li>	    		
+	      			<li><a href="team.php"><h4>Meet the Team</h4></a></li>
+	        		<li><a href="events.php"><h4>Events & Activities</h4></a></li>
+	        		<li><a href="register_main.php"><h4>Register</h4></a></li> 
+	        		<li><a href="faq.php"><h4>FAQs</h4></a></li>
+	      		</ul>	
+	    	</div>
+	    </div>
+	  </div>
+	</nav>	
+
+<br><br><br><br>
+<!-- Secretary -->
+<h2 align="center">Socials Secretaries</h2>
+<br>
+<center>
+<div class="container">
+
+<div class="col-md-2"></div>
+<div class="col-md-3">
+<img src="img/team/samruddhi.jpg" onclick="document.getElementById('sec1').style.display='block'" style="width:45%" class="w3-circle w3-hover-opacity">
+<h3 align="center">Samruddhi Mane</h3>
+<div id="sec1" class="w3-modal">
+<div class="col-md-4"></div>
+<div class="w3-modal-content col-md-4 w3-card-8 w3-animate-top">
+<header class="w3-teal" >
+<span onclick="document.getElementById('sec1').style.display='none'" class="w3-closebtn"><i class="fa fa-remove"></i></span>
+<br><br>
+<img src="img/team/samruddhi.jpg" align="center" style="width:35%" class="w3-circle"><br><br>
+<p class="w3-xlarge">Samruddhi Mane</p>
+<p align="center">(Socials Secretary)</p>
+</header>
+<br><div class="col-md-1"></div><div class="col-md-10">
+<p align="justify">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+<br></div><div class="col-md-1"></div>
+</div>
+</div>
+</div>
+
+<div class="col-md-2"></div>
+
+<div class="col-md-3">
+<img src="img/team/shivam.jpg" onclick="document.getElementById('sec2').style.display='block'" style="width:45%" class="w3-circle w3-hover-opacity">
+<h3 align="center">Shivam Walaskar</h3>
+
+<div id="sec2" class="w3-modal">
+<div class="col-md-4"></div>
+<div class="w3-modal-content col-md-4 w3-card-8 w3-animate-top">
+<header class="w3-teal" >
+<span onclick="document.getElementById('sec2').style.display='none'" class="w3-closebtn"><i class="fa fa-remove"></i></span>
+<br><br>
+<img src="img/team/shivam.jpg" align="center" style="width:35%" class="w3-circle"><br><br>
+<p class="w3-xlarge">Shivam Walaskar</p>
+<p align="center">(Socials Secretary)</p>
+</header>
+<br><div class="col-md-1"></div><div class="col-md-10">
+<p align="justify">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+<br></div><div class="col-md-1"></div>
+</div>
+</div>
+</div>
+
+<div class="col-md-2"></div>
+</center>
+<br><hr><br>
+<!-- Ex- Secretary -->
+<h2 align="center"><I>From the Diaries of Ex-Socials Secretaries</I></h2>
+<br>
+<center>
+<div class="container">
+
+<div class="col-md-4">
+<img src="img/team/nikhil.jpg" onclick="document.getElementById('exsec1').style.display='block'" style="width:35%" class="w3-circle w3-hover-opacity">
+<h3 align="center">Nikhil Badgu</h3>
+
+<div id="exsec1" class="w3-modal">
+<div class="col-md-4"></div>
+<div class="w3-modal-content col-md-4 w3-card-8 w3-animate-top">
+<header class="w3-teal" >
+<span onclick="document.getElementById('exsec1').style.display='none'" class="w3-closebtn"><i class="fa fa-remove"></i></span>
+<br><br>
+<img src="img/team/nikhil.jpg" align="center" style="width:35%" class="w3-circle"><br><br>
+<p class="w3-xlarge">Nikhil Badgu</p>
+<p align="center">(Ex-Socials Secretary)</p>
+</header>
+<br><div class="col-md-1"></div><div class="col-md-10">
+<p align="justify">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+<br></div><div class="col-md-1"></div>
+</div>
+</div>
+</div>
+
+<div class="col-md-4">
+<img src="img/team/priyanka.jpg" onclick="document.getElementById('exsec2').style.display='block'" style="width:35%" class="w3-circle w3-hover-opacity">
+<h3 align="center">Priyanka Wadekar</h3>
+
+<div id="exsec2" class="w3-modal">
+<div class="col-md-4"></div>
+<div class="w3-modal-content col-md-4 w3-card-8 w3-animate-top">
+<header class="w3-teal" >
+<span onclick="document.getElementById('exsec2').style.display='none'" class="w3-closebtn"><i class="fa fa-remove"></i></span>
+<br><br>
+<img src="img/team/priyanka.jpg" align="center" style="width:35%" class="w3-circle"><br><br>
+<p class="w3-xlarge">Priyanka Wadekar</p>
+<p align="center">(Ex-Socials Secretary)</p>
+</header>
+<br><div class="col-md-1"></div><div class="col-md-10">
+<p align="justify">&nbsp &nbsp &nbsp &nbsp Nothing can be more selfish to do selfless things to get utmost satisfaction. This can be experienced only after being part of <B>VIT Socials</B>. VIT Socials has been the best part of my life. I feel so privileged to be a part of such a great Team cum Family. The journey from being a volunteer to the secretary made me realize, what life really is. In the past 3 years, I have had a bunch of experiences, which I will cherish for the rest of my life. VIT Socials will always motivate me to do such things in future too.<br>&nbsp &nbsp &nbsp &nbsp I am very happy for Shivam  and Samruddhi. You guys deserve to be here. I am sure that this time  VIT Socials will break all the barriers of limitations and reach new heights. Make the impossible happen and make us proud. All the very best for your journey!</p>
+<br></div><div class="col-md-1"></div>
+</div>
+</div>
+</div>
+
+<div class="col-md-4">
+<img src="img/team/aakanksha.jpg" onclick="document.getElementById('exsec3').style.display='block'" style="width:35%" class="w3-circle w3-hover-opacity">
+<h3 align="center">Aakanksha Gaikwad</h3>
+
+<div id="exsec3" class="w3-modal">
+<div class="col-md-4"></div>
+<div class="w3-modal-content col-md-4 w3-card-8 w3-animate-top">
+<header class="w3-teal" >
+<span onclick="document.getElementById('exsec3').style.display='none'" class="w3-closebtn"><i class="fa fa-remove"></i></span>
+<br><br>
+<img src="img/team/aakanksha.jpg" align="center" style="width:35%" class="w3-circle"><br><br>
+<p class="w3-xlarge">Aakanksha Gaikwad</p>
+<p align="center">(Ex-Socials Secretary)</p>
+</header>
+<br><div class="col-md-1"></div><div class="col-md-10">
+<p align="justify">&nbsp &nbsp &nbsp &nbsp"<B>Socials</B>", not only a name but a family where you get a chance to self motivate and bring a change in the society. My 3 years in Team socials has groomed me into a better human being and more sensitive towards the society. These memories will be treasured by me for lifetime.<br>&nbsp &nbsp &nbsp &nbsp Wishing the Social Secretaries, Shivam and Samruddhi, all the good luck. You both will surely nurture our family and provide an experience for the new members which will be mesmerizing for the rest of their lives.</p>
+<br></div><div class="col-md-1"></div>
+</div>
+</div>
+</div>
+
+<div class="col-md-1"></div>
+</center>
+
+<div id="contact_us"></div>
+
+<br>
+<div id="contact">
+<h2 align="center" style="strong"><U>Contact Us</U></h2>
+<br>
+<h4 align="center">Contact us at: xyz@gmail.com</h4>
+<br><br><br>
+<footer id="footer">
+            <div class="row">
+                <div class="col md-6 col-sm-4">
+                    &copy; Copyright 2017 VIT Socials, PUNE 
+                </div>
+                <div class="col md-6 col-sm-8">
+                    <ul class="social-icons" align="right">
+                        <a href="https://www.facebook.com/vitsocial/"><i class="fa fa-facebook"></i></a>
+                        <a href="https://www.instagram.com/vitsocials"><i class="fa fa-instagram"></i></a></li>
+                        <a href="https://www.youtube.com/channel/UC8jDUVEpbsigF58K2ZLxKwA"><i class="fa fa-youtube"></i>
+  
+                    </ul>
+                </div>
+            </div>
+    </footer><!--/#footer-->
+</body>
+</html>
